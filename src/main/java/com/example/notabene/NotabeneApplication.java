aa1cdbf41700e07386dd140e672757e5c2ec807c
franks toscani <franks.koudjagochui@studio.unibo.it>
2025-07-22 12:48:03 +0200

    Setup con SpringBoot

---
+++ src/main/java/com/example/notabene/NotabeneApplication.java
@@ -0,0 +1,13 @@
+package com.example.notabene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotabeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotabeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notabene/NotabeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notabene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotabeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
