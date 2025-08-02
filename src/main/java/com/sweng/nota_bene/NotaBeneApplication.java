5b4a05dea4c7b38049f8cee6b3b3dab1c4e513a1
franks toscani <franks.koudjagochui@studio.unibo.it>
2025-08-02 18:16:56 +0200

    initial commit

---
+++ src/main/java/com/sweng/nota_bene/NotaBeneApplication.java
@@ -0,0 +1,13 @@
+package com.sweng.nota_bene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaBeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaBeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweng/nota_bene/NotaBeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweng.nota_bene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaBeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
